f227e77542f442499db2dbe5be94c2ed646ea8f9
Viphase <83181407+Ollie-Studio@users.noreply.github.com>
2023-08-21 12:26:30 -0400

    Delete HTML & JS directory

--- HTML & JS/Mini Apps/RNG/index.js
+++
@@ -1,8 +0,0 @@
-let num;
-
-document.getElementById("btn").onclick = function()
-{
-    console.log("click!");
-    num = Math.floor(Math.random() * 100);
-    document.getElementById("num").innerHTML = num;
-}
